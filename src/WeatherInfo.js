ee4e89fd340542a1c2d950c17e439efbf2ced96d
Keitumetse Mabalane <kmabalane023@student.wethinkcode.co.za>
2024-11-06 18:17:59 +0200

    updated Weatherinfo.js to add mor wether emojis

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,17 +5,36 @@ const WeatherInfo = ({ weather, temperature }) => {
 
   if (weather === "Clear") {
     weatherEmoji = "☀️";
-  } else if (weather === "Rain") {
-    weatherEmoji = "🌧️";
+  } else if (weather === "Partly Cloudy") {
+    weatherEmoji = "🌤️";
   } else if (weather === "Clouds") {
     weatherEmoji = "☁️";
+  } else if (weather === "Rain") {
+    weatherEmoji = "🌧️";
+  } else if (weather === "Thunderstorm") {
+    weatherEmoji = "🌩️";
+  } else if (weather === "Showers") {
+    weatherEmoji = "🌦️";
   } else if (weather === "Snow") {
     weatherEmoji = "❄️";
-  } else if (weather === "Thunderstorm") {
-    weatherEmoji = "⚡";
+  } else if (weather === "Fog") {
+    weatherEmoji = "🌫️";
+  } else if (weather === "Windy") {
+    weatherEmoji = "🌬️";
+  } else if (weather === "Hail") {
+    weatherEmoji = "🌨️";
+  } else if (weather === "Heatwave") {
+    weatherEmoji = "🌡️";
+  } else if (weather === "Dust Storm") {
+    weatherEmoji = "🌪️";
+  } else if (weather === "Mist") {
+    weatherEmoji = "🌁";
+  } else if (weather === "Drizzle") {
+    weatherEmoji = "🌦️";
   } else {
     weatherEmoji = "🌥️";
   }
+  
 
   return (
     <div className="weather-info">
